5110543f54c977f6e94678f6440d15db2ea5833d
Ryan McCaffrey <ryan.mccaffrey.42@gmail.com>
2017-11-28 17:13:46 -0500

    firefox quantum compatible

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
-
-// Create a parent item and two children.
-var parent = chrome.contextMenus.create({"title": "trill - Open on...", "contexts": ["selection"]});
-var child1 = chrome.contextMenus.create({"title": "dbr.ee", "contexts": ["selection"], "parentId": parent, "onclick": openOnDbree});
-var child2 = chrome.contextMenus.create({"title": "we.tl", "contexts": ["selection"], "parentId": parent, "onclick": openOnWetl});
-var child3 = chrome.contextMenus.create({"title": "mega.nz", "contexts": ["selection"], "parentId": parent, "onclick": openOnMega});
-var child4 = chrome.contextMenus.create({"title": "Google Drive", "contexts": ["selection"], "parentId": parent, "onclick": openOnGoogle});
-var child5 = chrome.contextMenus.create({"title": "Dropbox", "contexts": ["selection"], "parentId": parent, "onclick": openOnDropbox});
+// Create a parent item and children for each hosting site.
+var parent = browser.menus.create({title: "trill - Open on...", contexts: ["selection"]});
+browser.menus.create({title: "dbr.ee", contexts: ["selection"], parentId: parent, onclick: openOnDbree});
+browser.menus.create({title: "we.tl", contexts: ["selection"], parentId: parent, onclick: openOnWetl});
+browser.menus.create({title: "mega.nz", contexts: ["selection"], parentId: parent, onclick: openOnMega});
+browser.menus.create({title: "Google Drive", contexts: ["selection"], parentId: parent, onclick: openOnGoogle});
+browser.menus.create({title: "Dropbox", contexts: ["selection"], parentId: parent, onclick: openOnDropbox});
 
 function openOnDbree(info, tab) {
   const url = "https://dbr.ee/" + info.selectionText;
-  chrome.tabs.create({ url });
+  browser.tabs.create({ url });
 }
 
 function openOnWetl(info, tab) {
   const url = "https://we.tl/" + info.selectionText;
-  chrome.tabs.create({ url });
+  browser.tabs.create({ url });
 }
 
 function openOnMega(info, tab) {
   const url = "https://mega.nz/" + info.selectionText;
-  chrome.tabs.create({ url });
+  browser.tabs.create({ url });
 }
 
 function openOnGoogle(info, tab) {
   const url = "https://drive.google.com/file/d/" + info.selectionText;
-  chrome.tabs.create({ url });
+  browser.tabs.create({ url });
 }
 
 function openOnDropbox(info, tab) {
   const url = "https://dropbox.com/s/" + info.selectionText;
-  chrome.tabs.create({ url });
+  browser.tabs.create({ url });
 }
